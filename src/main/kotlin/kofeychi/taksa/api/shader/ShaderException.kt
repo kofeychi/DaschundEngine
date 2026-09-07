b46d86c47012d963f8de84d92a4aedbe3fc24317
kofeychi <138976045+kofeychi@users.noreply.github.com>
2026-09-07 20:38:07 +0100

    to rewrite

--- src/main/kotlin/kofeychi/taksa/api/shader/ShaderException.kt
+++
@@ -1,7 +0,0 @@
-package kofeychi.taksa.api.shader
-
-class ShaderException(
-    message: String
-) : RuntimeException(
-    message
-)
--- src/main/kotlin/kofeychi/taksa/api/shader/ShaderSource.kt
+++
@@ -1,5 +0,0 @@
-package kofeychi.taksa.api.shader
-
-@JvmInline value class ShaderSource(
-    val source: String,
-)
